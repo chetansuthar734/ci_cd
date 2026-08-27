f7f9a97332d08d60a3a869e2ad38acb3006aa692
chetan <chetansuthar734@gmail.com>
2026-08-27 22:17:47 +0530

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+async def root():
+    return {
+        "message": "Hello FastAPI"
+    }
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    return {
+        "user_id": user_id
+    }
